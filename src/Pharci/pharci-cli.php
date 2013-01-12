41ca7d9ef38159a10dc4ec90cd78ad33fab33642
bazinga <bazinga@bazingas-MacBook-Air.local>
2013-01-12 06:21:00 +0100

    rename

---
+++ src/Pharci/pharci-cli.php
@@ -0,0 +1,30 @@
+#!/usr/bin/php
+/*!
+* ------------------------------------------------------------------               
+* Pharci
+*
+* PHP Development Utility: PHAR Auto-Updater
+*
+* Copyright 2013, Gianni Furger <gianni.furger@gmail.com>
+* 
+* https://raw.github.com/alternatex/pharci/master/LICENSE
+*
+* ------------------------------------------------------------------ 
+*/
+ <?php
+
+// getopt helpers - params
+$options='';
+$longopts=array('dir', 'file', 'events');
+
+// getopt helpers - initialize
+array_map(function($option){
+	// set shortopt as first char o longopt (yeah, I know..)
+	$options.=substr($option, 0, 1).':';
+}, $longopts);
+
+// retrieve commandline args
+$args = getopt($options, $longopts);
+
+// run phar-update
+include(dirname(__FILE__).'/settings.php');
---
+++ src/Pharci/pharci-web.php
@@ -0,0 +1,18 @@
+<?php
+/*!
+* ------------------------------------------------------------------               
+* Pharci
+*
+* PHP Development Utility: PHAR Auto-Updater
+*
+* Copyright 2013, Gianni Furger <gianni.furger@gmail.com>
+* 
+* https://raw.github.com/alternatex/pharci/master/LICENSE
+*
+* ------------------------------------------------------------------ 
+*/
+
+// TODO: fetch params from $_REQUEST XXX
+
+// run phar-update
+include(dirname(__FILE__).'/settings.php');
