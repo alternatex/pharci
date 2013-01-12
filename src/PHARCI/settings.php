49f7a38e1991e98fbd9032113ca9bae3a2d7ed2c
bazinga <bazinga@bazingas-MacBook-Air.local>
2013-01-12 06:20:05 +0100

    rename

--- src/PHARCI/settings.php
+++
@@ -1,6 +0,0 @@
-<?php 
-// path to settings
-$settings = 'settings.json'; 
-
-// read $settings and extract it's contents as variables 
-if(file_exists($settings)) extract(json_decode(file_get_contents($settings),true)); else die("ERROR: settings.json not found.");
